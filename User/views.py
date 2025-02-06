16f916e18c47572f152d6448d3d882ba63c743f7
xusanboyman2008 <abdulkhaevtolibjon55@gmail.com>
2025-02-06 20:22:24 +0500

    j

--- User/views.py
+++ User/views.py
@@ -7,9 +7,7 @@ from .models import User
 def get_user_role_and_tg_id(request):
     if request.method == "POST":
         token = request.POST.get("token")
-        print(token)
         user = User.objects.get(token=token)
-        print(user.id)
         if user:
             return JsonResponse({'id':user.id,'tg_id': user.tg_id, 'role': user.role},status=200)
         return JsonResponse({'message':'error'},status=404)
