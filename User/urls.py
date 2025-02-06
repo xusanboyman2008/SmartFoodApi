b47a4d433ce1ff1a209ca712798914758c1631e1
xusanboyman2008 <abdulkhaevtolibjon55@gmail.com>
2025-02-06 19:06:21 +0500

    j

--- User/urls.py
+++ User/urls.py
@@ -3,6 +3,5 @@ from django.urls import  path
 from User import views
 
 urlpatterns = [
-    path('', views.user, name='home'),
     path('get_token/<token>',views.get_user_role_and_tg_id,name='get_user_role_and_tg_id'),
 ]
--- User/views.py
+++ User/views.py
@@ -4,13 +4,6 @@ from .models import User
 
 
 # Create your views here.
-
-def user(request):
-    if request.method == "GET":
-        users = User.objects.all()
-        users_data = [{'id': user.id, 'tg_id': user.tg_id} for user in users]
-        return JsonResponse({'users': users_data})
-
 def get_user_role_and_tg_id(request,token):
     if request.method == "POST":
         user = User.objects.get(token=token)
