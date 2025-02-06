5569c687f5badbd8ab985fa65f396c242e9ce3e5
xusanboyman2008 <abdulkhaevtolibjon55@gmail.com>
2025-02-06 19:20:10 +0500

    j

--- User/urls.py
+++ User/urls.py
@@ -3,5 +3,5 @@ from django.urls import  path
 from User import views
 
 urlpatterns = [
-    path('get_token/<token>',views.get_user_role_and_tg_id,name='get_user_role_and_tg_id'),
+    path('get_token/',views.get_user_role_and_tg_id,name='get_user_role_and_tg_id'),
 ]
